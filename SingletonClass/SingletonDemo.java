01ca0314f3601cc6ba6e55ee91b7628d507c32a9
JuileePanse <58484814+JuileePanse@users.noreply.github.com>
2020-03-19 13:21:54 -0700

    Add files via upload
    Singleton class can only create one object

---
+++ SingletonClass/SingletonDemo.java
@@ -0,0 +1,8 @@
+public class SingletonDemo {
+    public static void main(String[] args) {
+       Singleton tmp = Singleton.getInstance( );
+       // Display
+       //   demoMethod for singleton
+       tmp.demoMethod( );
+    }
+ }
